aa8de92babd7c417523fcec1aedcb56dc19d792d
daniyal-p <daniyal733khan@gmail.com>
2025-02-26 08:21:37 +0530

    Homepage UI updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+        extend: {
+          colors: {
+            vvlight: '#7FD1E0',  // Light blue from logo top
+            vvmedium: '#6A96D7', // Medium blue from logo middle
+            vvdark: '#896AD7',   // Purple from logo bottom
+          }
+        }
+      },
 	plugins: [],
 }
